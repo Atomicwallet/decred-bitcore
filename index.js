19cd49e5c0248fda4783ce929683281a85db8cab
lev.akhnazarov <levakhnazarov@gmail.com>
2019-02-21 20:42:23 +0300

    fix vg

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ var bitcore = module.exports;
 
 // module information
 bitcore.version = 'v' + require('./package.json').version;
-global._bitcore = bitcore.version;
+bitcore.versionGuard = function(version) {
+  if (version !== undefined) {
+    var message = 'More than one instance of bitcore-lib found. ' +
+      'Please make sure to require bitcore-lib and check that submodules do' +
+      ' not also include their own bitcore-lib dependency.';
+    throw new Error(message);
+  }
+};
+bitcore.versionGuard(global._decredbitcore);
+global._decredbitcore = bitcore.version;
 
 // crypto
 bitcore.crypto = {};
